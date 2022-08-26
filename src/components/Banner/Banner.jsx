e96df5b77f5fe46a93eb12b54f5337868f0252c6
n00rs <007s.noorsha@gmail.com>
2022-08-27 00:41:46 +0530

    added nabar

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className='content'>
+                <h1 className='title'>Movie</h1>
+                <div className="banner-btn">
+                    <button className="btn">Play</button>
+                    <button className="btn">My List</button>
+                </div>
+                <h1 className="description">hi iam desciprtion  iam from html css and javascript jqeury ajax </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <img className='nav-logo'  src="/images/Netflix_log.png" alt=""  />
+        <img className='nav-avatar' src="/images/navAvatar.png" alt=""  />
+    </div>
+  )
+}
+
+export default Navbar
